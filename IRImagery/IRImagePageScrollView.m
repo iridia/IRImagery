124e8257c01143ef21ed5ab1e5a58ba8d1980bee
Evadne Wu <ev@radi.ws>
2012-11-15 02:41:31 -0800

    IRImagePageScrollView respects content insets a bit more

--- IRImagery/IRImagePageScrollView.m
+++ IRImagery/IRImagePageScrollView.m
@@ -21,18 +21,19 @@
 	if (tileContainerView) {
 	
 		CGSize boundsSize = self.bounds.size;
+		boundsSize.width -= self.contentInset.left + self.contentInset.right;
+		boundsSize.height -= self.contentInset.top + self.contentInset.bottom;
+		
 		CGRect frameToCenter = tileContainerView.frame;
 
-		if (frameToCenter.size.width < boundsSize.width)
-			frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2;
-		else
-			frameToCenter.origin.x = 0;
-
-		if (frameToCenter.size.height < boundsSize.height)
-			frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2;
-		else
-			frameToCenter.origin.y = 0;
-
+		frameToCenter.origin.x = (frameToCenter.size.width < boundsSize.width) ?
+			(boundsSize.width - frameToCenter.size.width) / 2 :
+			0;
+			
+		frameToCenter.origin.y = (frameToCenter.size.height < boundsSize.height) ?
+			(boundsSize.height - frameToCenter.size.height) / 2 :
+			0;
+			
 		tileContainerView.frame = frameToCenter;
 	
 	}
